b95655f1074e3cf881427f98483c357c3db1562e
Pierre Mesure <pierre@mesu.re>
2020-05-20 10:15:15 +0200

    Remove regions from the form

--- app/services/socio_demographic_authorization_handler.rb
+++ app/services/socio_demographic_authorization_handler.rb
@@ -2,16 +2,11 @@
 
 # Allows to create a form for simple Socio Demographic authorization
 class SocioDemographicAuthorizationHandler < Decidim::AuthorizationHandler
-  attribute :scope_id, Integer
   attribute :gender, String
   attribute :age, String
 
   GENDER = %w(woman man undefined).freeze
   AGE_SLICE = %w(16-25 26-45 46-65 65+).freeze
-
-  validates :scope_id,
-            format: { with: /\A\d+\z/, message: I18n.t("errors.messages.integer_only"), if: proc { |x| !x.scope_id.nil? && validate_scope } },
-            presence: false
 
   validates :gender,
             inclusion: { in: GENDER, if: proc { |x| x.gender.present? } },
@@ -22,12 +17,11 @@ class SocioDemographicAuthorizationHandler < Decidim::AuthorizationHandler
             presence: false
 
   def metadata
-    super.merge(scope_id: scope_id, gender: gender, age: age)
+    super.merge(gender: gender, age: age)
   end
 
   private
 
   def validate_scope
-    errors.add(:scope_id, :invalid) if Decidim::Scope.where(id: scope_id).empty?
   end
 end
