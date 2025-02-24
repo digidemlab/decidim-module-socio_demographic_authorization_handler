eed104552b72c4047a8b5bc8be2ae9ac45aa204f
Petter <petter@digidemlab.org>
2025-02-24 20:30:22 +0100

    Remove phone number field

--- app/services/socio_demographic_authorization_handler.rb
+++ app/services/socio_demographic_authorization_handler.rb
@@ -5,7 +5,6 @@ class SocioDemographicAuthorizationHandler < Decidim::AuthorizationHandler
   attribute :participatory_space, String
   attribute :gender, String
   attribute :age, String
-  attribute :phone_number, String
   attribute :living_area, String
 
   GENDER_OPTIONS = Decidim::SocioDemographicAuthorizationHandler::StaticAuthorizationData.genders.map(&:downcase).freeze
@@ -15,7 +14,6 @@ class SocioDemographicAuthorizationHandler < Decidim::AuthorizationHandler
   validate :validate_participatory_space
   validates :gender, inclusion: { in: GENDER_OPTIONS }, allow_blank: true
   validates :age, inclusion: { in: AGE_OPTIONS }, allow_blank: true
-  validates :phone_number, format: { with: /\A\+?\d{7,20}\z/ }, allow_blank: true
   validates :living_area, inclusion: { in: LIVING_AREA_OPTIONS }, allow_blank: true
 
   def metadata
@@ -23,7 +21,6 @@ class SocioDemographicAuthorizationHandler < Decidim::AuthorizationHandler
       participatory_space: participatory_space.presence,
       gender: gender.presence,
       age: age.presence,
-      phone_number: phone_number.presence,
       living_area: living_area.presence
     }.compact
   end
