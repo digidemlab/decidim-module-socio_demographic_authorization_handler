f1fd53ee792f422378a970cda555234115474a63
Rupus Reinefjord <rupus@digidemlab.org>
2025-04-16 11:19:48 +0000

    Revert "temporary fix with verifications, only register one"
    This reverts commit bfbbe82005bb74e1ed9fc668976d7fb1d148503b.

--- lib/decidim/socio_demographic_authorization_handler/workflow.rb
+++ lib/decidim/socio_demographic_authorization_handler/workflow.rb
@@ -4,6 +4,6 @@ Decidim::Verifications.register_workflow(:socio_demographic_authorization_handle
   workflow.form = "SocioDemographicAuthorizationHandler"
 end
 
-#Decidim::Verifications.register_workflow(:socio_demographic_no_phone_authorization_handler) do |workflow|
-#  workflow.form = "SocioDemographicNoPhoneAuthorizationHandler"
-#end
+Decidim::Verifications.register_workflow(:socio_demographic_no_phone_authorization_handler) do |workflow|
+  workflow.form = "SocioDemographicNoPhoneAuthorizationHandler"
+end
