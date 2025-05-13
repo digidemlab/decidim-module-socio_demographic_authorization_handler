c1fe441b41956cea125cdc1ce31c6f10fa2b45fe
Rupus Reinefjord <rupus@digidemlab.org>
2025-05-13 10:09:16 +0000

    typo...

--- app/serializers/concerns/decidim/socio_demographic_authorization_handler/proposal_serializer_extensions.rb
+++ app/serializers/concerns/decidim/socio_demographic_authorization_handler/proposal_serializer_extensions.rb
@@ -23,7 +23,7 @@ module Decidim
               author_email(author)
             end,
             phone: resource.authors.map do |author|
-              authorization = Decidim::Authorization.where(decidim_user_id: author.id, name: "socio_demograhpic_authorization_handler").last
+              authorization = Decidim::Authorization.where(decidim_user_id: author.id, name: "socio_demographic_authorization_handler").last
               authorization.nil? ? "" : authorization.metadata["phone_number"]
             end
           }
