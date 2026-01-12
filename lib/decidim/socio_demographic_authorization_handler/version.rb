cc0be8d6a8635de043ed585468b6aa92745c35da
Rupus Reinefjord <rupus@digidemlab.org>
2026-01-12 14:06:19 +0100

    v0.30.0

--- lib/decidim/socio_demographic_authorization_handler/version.rb
+++ lib/decidim/socio_demographic_authorization_handler/version.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 module Decidim
-  # This holds the decidim-meetings version.
   module SocioDemographicAuthorizationHandler
-    VERSION = "0.29"
-    DECIDIM_VERSION = "0.29.1"
-    COMPAT_DECIDIM_VERSION = [">= 0.29.0", "< 0.30"].freeze
+    VERSION = "0.30"
+    DECIDIM_VERSION = "0.30.0"
+    COMPAT_DECIDIM_VERSION = [">= 0.30.0", "< 0.31"].freeze
   end
 end
